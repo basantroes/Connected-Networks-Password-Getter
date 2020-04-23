b958feec657db15a5be4018ee2ba7445ab89f056
Ammar Yasser AllaiThy <AmmarYasserAllaiThy@gmail.com>
2020-04-23 05:46:30 +0200

    Just formatting

--- Main.java
+++ Main.java
@@ -1,26 +1,21 @@
-import javax.sound.midi.SysexMessage;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class Main {
     public static Process p;
 
     public static void showPassword(String networkName) {
-
         String command = "cmd.exe /c  netsh wlan show profiles key=clear name=" + networkName;
         try {
             p = Runtime.getRuntime().exec(command);
             Scanner cs = new Scanner(p.getInputStream());
             String line = "";
+
             while (cs.hasNext()) {
                 line += cs.nextLine();
                 line += "\n";
             }
-
 
             String s = line.substring(line.indexOf("Key Content") + 25);
             System.out.print("Password: " + s.substring(0, s.indexOf("\n")));
@@ -37,23 +32,21 @@ public class Main {
             p = Runtime.getRuntime().exec("cmd.exe /c netsh wlan show profiles");
             Scanner scs = new Scanner(p.getInputStream());
             String line = "";
-            int lineNo=0;
+            int lineNo = 0;
+
             while (scs.hasNext()) {
                 line += scs.nextLine() + "\n";
-               lineNo++;
-
+                lineNo++;
             }
-            for(int i=0; i<lineNo+1;i++){
-                if(line.contains("All User Profile")) {
+
+            for (int i = 0; i < lineNo + 1; i++) {
+                if (line.contains("All User Profile")) {
                     String recS = line.substring(line.indexOf("All User Profile") + 22);
                     allNetworks.add(recS.substring(0, recS.indexOf("\n")));
                     System.out.println(allNetworks.get(i));
                     line = recS;
-                }
-                else break;
-
+                } else break;
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -64,14 +57,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("All connected networks are: ");
         showAllConnectedNetworks();
-        while(true){
-        System.out.println("Enter the  network name from the previous list: ");
-        Scanner scan = new Scanner(System.in);
-         String name = scan.next();
-         showPassword(name);
-        System.out.print("\n");
 
-
-
+        while (true) {
+            System.out.println("Enter the  network name from the previous list: ");
+            Scanner scan = new Scanner(System.in);
+            String name = scan.next();
+            showPassword(name);
+            System.out.print("\n");
+        }
     }
-}}
+}
